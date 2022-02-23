3f7b20d9c7e4d103b302da8a9807f055288abc79
ChisakiNakamura <cnakamura1@my.bcit.ca>
2022-02-23 06:41:38 -0800

    mouse animation working !!!

--- portfolio/comp/Mouse/index.js
+++ portfolio/comp/Mouse/index.js
@@ -2,23 +2,38 @@ import useMousePosition from "../../hooks/useMousePosition";
 import React, {useContext, useState,useEffect } from "react";
 import { MouseContext } from "../../utils/mouse-context";
 import styled, {keyframes} from 'styled-components';
-
+/*
 const Dot = styled.div`
-position:absolute;
-width: 8px;
-height: 8px;
+width: 10px;
+height: 10px;
 background-color: #EBEEF6;
 border-radius: 100%;
-z-index: 900;
+z-index: 999;
 pointer-events: none;
+display:flex;
+margin-right:10px;
 `
+const Box = styled.div`
+display:flex;
+justify-content:flex-end;
+`*/
 const Cursor = styled.div`
+position:absolute;
+width: 10px;
+height: 10px;
+background-color: #EBEEF6;
+border-radius: 100%;
 will-change: transform;
-z-index: 900;
+transform: translate(-999px, -999px);
+z-index: 999;
 pointer-events: none;
+
 `
+// margin-right:10px;
 const Img = styled.img`
-position:fixed;
+position:relative;
+top:10px;
+left:-75px;
 width:80px;
 height:80px;
 z-index: 999;
@@ -26,6 +41,8 @@ pointer-events: none;
 transform:scale(${props=>props.scale});
 will-change: transform;
 transition: transform .1s linear;
+transform: translate(-999px, -999px);
+transform-origin: top right;
 -webkit-transition-duration: 100ms;
 transition-duration: 200ms;
 -webkit-transition-timing-function: ease-out;
@@ -39,27 +56,108 @@ const Mouse = (
   
 ) =>{
  
-  const { x, y } = useMousePosition();
+  //const { x, y } = useMousePosition();
   const [cursor] = useContext(MouseContext);
-  const cursorAnim = keyframes`
-    0% {top:${y}px;}
-    50% {top:${y + 12}px;}
-    100% {top:${y}px;}
-    `
-  return<Dot
-  style={{ left: `${x}px`, top: `${y}px` }}
-  ><Cursor
+  
+  /*const cursorAnim = keyframes`
+  0% {transform:translate(${x},${y}px);}
+  50% {transform:translate(${x},${y + 12}px);}
+  100% {transform:translate(${x},${y}px);}
+  `*/
+  
+  
+    var rotatingCursor = function() {
+
+      /* Local Variables */
+      const INTERVAL_POSITION = 5;
+      const INTERVAL_ROTATION = 70;
+      let lastCursorPos = {x: -999, y: -999};
+      let currentCursorPos = {x: -999, y: -999};
+      let lastCursorAngle = 0, cursorAngle = 0;
+      let cursorEl, cursorImageEl;//,cursorDot;
+      /* Local Functions */
+    
+      // NOTE: I am transform two different elements here because so I can only animate the rotation with 'transition-property: transform'.
+      function setCurrentCursorProps() {
+        // Apply translation (set to actual cursor position)
+        cursorEl.style.transform = `translate(${currentCursorPos.x}px, ${currentCursorPos.y}px)`;
+        //cursorImageEl.style.transform =`translate(${currentCursorPos.x}px, ${currentCursorPos.y}px)`;
+        // Ensure correct rotation transition direction
+        while (Math.abs(lastCursorAngle - cursorAngle) > 180) {
+          if (cursorAngle > lastCursorAngle) {
+            cursorAngle -= 360;
+          } else if (cursorAngle < lastCursorAngle) {
+            cursorAngle += 360;
+          }
+        }
+        // Apply rotation
+       //cursorDot.style.transform = `rotate(${cursorAngle - 90}deg)`;
+        cursorImageEl.style.transform = `rotate(${cursorAngle - 90}deg)`;
+        //cursorEl.style.transform = `rotate(${cursorAngle - 90}deg)`;
+      }
+    
+      function updateCursor() {
+        window.addEventListener('mousemove', event => {
+          currentCursorPos = {x: event.clientX, y: event.clientY};
+     
+    
+        });
+        console.log(currentCursorPos)
+        // Interval for updating cursor-position
+        setInterval(setCurrentCursorProps, INTERVAL_POSITION);
+    
+        // Interval for updating cursor-rotation
+        setInterval(() => {
+          const delt = {
+            x: lastCursorPos.x - currentCursorPos.x,
+            y: lastCursorPos.y - currentCursorPos.y
+          }
+          if (Math.abs(delt.x) < 3 && Math.abs(delt.y) < 3) return;
+          cursorAngle = (Math.atan2(delt.y, delt.x) * 180 / Math.PI);
+    
+          setCurrentCursorProps();
+          
+          lastCursorPos = currentCursorPos;
+          lastCursorAngle = cursorAngle;
+
+          
+        }, INTERVAL_ROTATION);
+       
+      }
+      
+    
+      /* Public Functions */
+    
+          cursorEl = document.querySelector('.cursor');
+          cursorImageEl = document.querySelector('.myImg');
+          //cursorDot = document.querySelector('.myDot');
+          updateCursor();
+          
+    }
+   
+    useEffect(() => {
+     
+       rotatingCursor()
+       
+      return () => {
+      };
+      
+    }, []);
+   
+  return<Cursor
   className="cursor"
-  style={{transform:`translate(${x}px,${y}px)`}}
+  //style={{ left: `${x}px`, top: `${y}px` }}
+  //style={{transform:`translate(${x}px,${y}px)`}}
   ><Img 
-  style={{ left: `${x}px`, top: `${y}px` }}
+  //style={{ left: `${x}px`, top: `${y}px` }}
+  //style={{transform:`translate(${x}px,${y}px)`}}
   src="./roket_mouse.svg" 
-  animation={cursorAnim}
+  //animation={cursorAnim}
   scale={cursor.active ?'1.7':'1'}
   className="myImg"
   /> 
   </Cursor>
-  </Dot>
+  
   
 }
 
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -166,84 +166,11 @@ export default function Home() {
   }
   const cursorHandlers = useCursorHandlers();
 
- var rotatingCursor = function() {
-
-  /* Local Variables */
-  const INTERVAL_POSITION = 5;
-  const INTERVAL_ROTATION = 70;
-  let lastCursorPos = {x: -999, y: -999};
-  let currentCursorPos = {x: -999, y: -999};
-  let lastCursorAngle = 0, cursorAngle = 0;
-  let cursorEl, cursorImageEl;
-  /* Local Functions */
-
-  // NOTE: I am transform two different elements here because so I can only animate the rotation with 'transition-property: transform'.
-  function setCurrentCursorProps() {
-    // Apply translation (set to actual cursor position)
-    cursorEl.style.transform = `translate(${currentCursorPos.x}px, ${currentCursorPos.y}px)`;
-
-    // Ensure correct rotation transition direction
-    while (Math.abs(lastCursorAngle - cursorAngle) > 180) {
-      if (cursorAngle > lastCursorAngle) {
-        cursorAngle -= 360;
-      } else if (cursorAngle < lastCursorAngle) {
-        cursorAngle += 360;
-      }
-    }
-    // Apply rotation
-    cursorImageEl.style.transform = `rotate(${cursorAngle - 90}deg)`;
-  }
-
-  function updateCursor() {
-    window.addEventListener('mousemove', event => {
-      currentCursorPos = {x: event.clientX, y: event.clientY};
- 
-
-    });
-
-    // Interval for updating cursor-position
-    setInterval(setCurrentCursorProps, INTERVAL_POSITION);
-
-    // Interval for updating cursor-rotation
-    setInterval(() => {
-      const delt = {
-        x: lastCursorPos.x - currentCursorPos.x,
-        y: lastCursorPos.y - currentCursorPos.y
-      }
-      if (Math.abs(delt.x) < 3 && Math.abs(delt.y) < 3) return;
-      cursorAngle = (Math.atan2(delt.y, delt.x) * 180 / Math.PI);
-
-      setCurrentCursorProps();
-
-      lastCursorPos = currentCursorPos;
-      lastCursorAngle = cursorAngle;
-    }, INTERVAL_ROTATION);
-    
-  }
-  
-
-  /* Public Functions */
-
-      cursorEl = document.querySelector('.cursor');
-      cursorImageEl = document.querySelector('.myImg');
-      console.log(cursorImageEl)
-      updateCursor();
-}
-
-useEffect(() => {
- 
-   rotatingCursor()
-  return () => {
-  };
-  
-}, []);
 
   return (
       <MainBox>
         {/* ================= Home ====================== */}
         <Section id="home">
-          <Mouse
-          />
           <HeaderBox>
             <Nav onMenuClick={toggle}/>
           </HeaderBox>
